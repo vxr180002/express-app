7567cdcd79bbd0f57f932f23aa6fb5678364b751
vxr180002 <vxr180002@utdallas.edu>
2019-03-24 00:46:38 -0600

    adding a simple console log for list

--- routes/list.js
+++ routes/list.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var router = express.Router()
 
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+    console.log('Time: ', Date.now())
+    next()
+})
+
 router.get('/', function(req, res){
     res.send('list all items')
 })
